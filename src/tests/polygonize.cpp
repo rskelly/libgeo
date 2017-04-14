454957b19fb9b9495ba22f827980a2f93ef59690
rob <rob@dijital.ca>
2017-04-13 23:13:57 -0700

    Added polygons test and some sample data; set up linkage for interprocess.

---
+++ src/tests/polygonize.cpp
@@ -0,0 +1,20 @@
+/*
+ * polygonize.cpp
+ *
+ *  Created on: Apr 13, 2017
+ *      Author: rob
+ */
+
+#include "raster.hpp"
+
+int main(int argc, char** argv) {
+
+	using namespace geo::raster;
+
+	Raster test("/home/rob/Documents/git/libgeo/data/tests/polygons.tif");
+	test.polygonize("/home/rob/Documents/git/libgeo/data/tests/output/polygons.sqlite", "polygons", "sqlite", 26910, 1, nullptr, nullptr);
+
+	return 0;
+}
+
+
