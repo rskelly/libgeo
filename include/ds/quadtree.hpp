88cad87ebeaa093b9a9e39c8ecdc4a637b47d246
rob <rob@dijital.ca>
2017-04-16 14:11:03 -0700

    Added index for retrieval/update. Removed internal array cache.

--- include/ds/quadtree.hpp
+++ include/ds/quadtree.hpp
@@ -10,6 +10,11 @@
 
 #include "util.hpp"
 
+#include <unordered_set>
+#include <unordered_map>
+#include <vector>
+#include <list>
+
 using namespace geo::util;
 
 namespace geo {
@@ -168,23 +173,32 @@ public:
 
 };
 
-static size_t s_fqtnOffset = 0;
+static uint64_t s_id = 0;
+
+static uint64_t s_nextId() {
+	uint64_t id = s_id;
+	++s_id;
+	return id;
+}
+
+template <class T>
+class FileQuadTree;
 
 template <class T>
 class FileQuadTreeNode {
-private:
+	friend class FileQuadTree<T>;
+protected:
 
-	typedef std::tuple<double, double, T> Item;
+	typedef std::tuple<uint64_t, double, double, T> Item;
 
-	MappedFile& m_data;
-	std::vector<size_t> m_offsets;
-	std::vector<Item> m_items;
-	geo::util::Bounds m_bounds;
 	std::unordered_map<char, std::unique_ptr<FileQuadTreeNode<T> > > m_nodes;
-	size_t m_count;
-	size_t m_cacheSize;
+	std::vector<size_t> m_offsets;
+	geo::util::Bounds m_bounds;
+	MappedFile& m_data;
 	int m_depth;
 	int m_maxDepth;
+	size_t m_count;
+	size_t m_cacheSize;
 
 	uint8_t idx(double x, double y) {
 		uint8_t i = 0;
@@ -219,43 +233,76 @@ private:
 		return m_nodes[i].get();
 	}
 
-protected:
 	FileQuadTreeNode(MappedFile& data, const geo::util::Bounds& bounds,
 			int depth, int maxDepth, int cacheSize) :
-		m_data(data),
 		m_bounds(bounds),
+		m_data(data),
+		m_depth(depth), m_maxDepth(maxDepth),
 		m_count(0),
-		m_cacheSize(cacheSize),
-		m_depth(depth), m_maxDepth(maxDepth) {
-		m_items.resize(m_cacheSize);
+		m_cacheSize(cacheSize) {
 	}
 
 public:
 
-	void add(double x, double y, const T& item) {
+	// Adds an item to the tree at the given position; returns
+	// the item's ID.
+	uint64_t addItem(double x, double y, const T& item) {
 		if(m_depth < m_maxDepth) {
-			getNode(x, y)->add(x, y, item);
+			return getNode(x, y)->addItem(x, y, item);
 		} else {
-			m_items[m_count % m_cacheSize] = std::make_tuple(x, y, item);
-			++m_count;
-			if(m_count > 0 && m_count % m_cacheSize == 0) {
-				size_t fOffset = s_fqtnOffset++;
-				size_t size = m_cacheSize * sizeof(Item);
-				size_t offset = fOffset * size;
-				m_data.reset((uint64_t) size * (fOffset + 1), true);
-				char* fdata = (char*) m_data.data();
-				char* vdata = (char*) m_items.data();
-				std::memcpy(fdata + offset, vdata, size);
-				m_offsets.push_back(fOffset);
+			// The size of a "row" in mapped memory.
+			size_t size = m_cacheSize * sizeof(Item);
+			// The offset into the mapped memory for the start of the current cache.
+			size_t offset;
+
+			// If the count has surpassed the size of the current block,
+			// get a new offset and start a new block.
+			if(m_count % m_cacheSize == 0) {
+				offset = s_nextId();
+				m_offsets.push_back(offset);
+				// Resize the memory if necessary.
+				m_data.reset(offset * size + size);
+			} else {
+				 offset = m_offsets[m_offsets.size() - 1];
 			}
+
+			// The position of the new item.
+			size_t position = offset * size + (m_count % m_cacheSize) * sizeof(Item);
+
+			// Write the item.
+			Item i = std::make_tuple(position, x, y, item);
+			char* fdata = (char*) m_data.data() + position;
+			std::memcpy(fdata, &i, sizeof(Item));
+
+			++m_count;
+
+			return position;
 		}
+	}
+
+	// Get an item from the tree.
+	bool getItem(Item& item, uint64_t id) {
+		if(id + sizeof(Item) > m_data.size())
+			return false;
+		char* data = (char*) m_data.data() + id;
+		std::memcpy(&item, data, sizeof(Item));
+		return true;
+	}
+
+	// Update the item in the tree.
+	bool updateItem(const Item& item, uint64_t id) {
+		if(id + sizeof(Item) > m_data.size())
+			return false;
+		char* data = (char*) m_data.data() + id;
+		std::memcpy(data, &item, sizeof(Item));
+		return true;
 	}
 
 	int count() const {
 		if(m_depth < m_maxDepth) {
 			int c = 0;
 			for(const auto& it : m_nodes)
-				c += it.second.count();
+				c += it.second->count();
 			return c;
 		} else {
 			return m_count;
@@ -267,31 +314,18 @@ public:
 		int count = 0;
 		if(m_bounds.intersects(bounds)) {
 			if(m_depth == m_maxDepth) {
-				// If there are items in memory, process them first.
-				if(m_count % m_cacheSize != 0) {
-					for(size_t j = 0; j < m_count % m_cacheSize; ++j) {
-						const Item& it = m_items[j];
-						double x = std::get<0>(it);
-						double y = std::get<1>(it);
-						if(bounds.contains(x, y)) {
-							*inserter = it;
-							++inserter;
-							++count;
-						}
-					}
-				}
 				// Move on to check items in mapped memory.
 				std::vector<Item> items(m_cacheSize);
 				size_t size = m_cacheSize * sizeof(Item);
 				for(size_t i = 0; i < m_offsets.size(); ++i) {
-					size_t offset = m_offsets[i];
-					char* fdata = (char*) m_data.data();
+					size_t offset = m_offsets[i] * size;
+					char* fdata = (char*) m_data.data() + offset;
 					char* vdata = (char*) items.data();
-					std::memcpy(vdata, fdata + offset * size, size);
-					for(size_t j = 0; j < m_count - i * m_cacheSize; ++j) {
-						const Item& it = m_items[j];
-						double x = std::get<0>(it);
-						double y = std::get<1>(it);
+					std::memcpy(vdata, fdata, size);
+					for(size_t j = 0; j < g_min(m_cacheSize, m_count - i * m_cacheSize); ++j) {
+						const Item& it = items[j];
+						double x = std::get<1>(it);
+						double y = std::get<2>(it);
 						if(bounds.contains(x, y)) {
 							*inserter = it;
 							++inserter;
@@ -311,9 +345,9 @@ public:
 	public:
 		double x, y;
 		Sorter(double x, double y) : x(x), y(y) {}
-		bool operator()(std::tuple<double, double, T>& a, std::tuple<double, double, T>& b) {
-			double da = g_sq(x - std::get<0>(a)) + g_sq(y - std::get<1>(a));
-			double db = g_sq(x - std::get<0>(b)) + g_sq(y - std::get<1>(a));
+		bool operator()(std::tuple<uint64_t, double, double, T>& a, std::tuple<uint64_t, double, double, T>& b) {
+			double da = g_sq(x - std::get<1>(a)) + g_sq(y - std::get<2>(a));
+			double db = g_sq(x - std::get<1>(b)) + g_sq(y - std::get<2>(a));
 			return da < db;
 		}
 	};
@@ -356,10 +390,48 @@ template <class T>
 class FileQuadTree : public FileQuadTreeNode<T> {
 private:
 	MappedFile m_data;
+	std::vector<FileQuadTreeNode<T>*> m_inodes;
+	size_t m_idx;
+	size_t m_nidx;
+	FileQuadTreeNode<T>* m_iqt;
 
 public:
 	FileQuadTree(const geo::util::Bounds& bounds, int maxDepth, int cacheSize = 4096) :
-		FileQuadTreeNode<T>(m_data, bounds, 0, maxDepth, cacheSize) {
+		FileQuadTreeNode<T>(m_data, bounds, 0, maxDepth, cacheSize),
+		m_idx(0), m_nidx(0), m_iqt(nullptr) {
+	}
+
+
+	void reset() {
+		m_idx = 0;
+		m_nidx = 0;
+		m_inodes.clear();
+		std::queue<FileQuadTreeNode<T>*> q;
+		m_iqt = this;
+		q.push(m_iqt);
+		while(!q.empty()) {
+			FileQuadTreeNode<T>* t = q.front();
+			q.pop();
+			if(t->m_depth == t->m_maxDepth) {
+				m_inodes.push_back(t);
+			} else {
+				for(auto& p : t->m_nodes)
+					q.push(p.second.get());
+			}
+		}
+	}
+
+	bool next(std::tuple<uint64_t, double, double, T>& item) {
+		while(m_nidx < m_inodes.size()) {
+			if(m_idx < m_inodes[m_nidx]->m_items.size()) {
+				std::memcpy(&item, &(m_inodes[m_nidx]->m_items[m_idx]), sizeof(std::tuple<uint64_t, double, double, T>));
+				++m_idx;
+				return true;
+			} else {
+				++m_nidx;
+			}
+		}
+		return false;
 	}
 };
 
--- src/tests/quadtree.cpp
+++ src/tests/quadtree.cpp
@@ -8,7 +8,7 @@
 #include "ds/quadtree.hpp"
 #include "util.hpp"
 
-	int main(int argc, char** argv) {
+int main(int argc, char** argv) {
 
 	using namespace geo::util;
 	using namespace geo::ds;
@@ -17,16 +17,29 @@
 
 	FileQuadTree<char> qt(b, 1, 100);
 
-	for(int i = 0; i < 1000; ++i) {
-		double x = Util::random(0.0, 100.0);
-		double y = Util::random(0.0, 100.0);
-		char a = (char) Util::random(0, 65);
+	std::vector<uint64_t> ids;
+	for(int i = 0; i < 10; ++i) {
+		double x = Util::random(0.0, 10.0);
+		double y = Util::random(0.0, 10.0);
+		char a = (char) Util::random(65, 91);
 		std::cerr << x << ", " << y << "\n";
-		qt.add(x, y, a);
+		ids.push_back(qt.addItem(x, y, a));
 	}
 
+	std::cerr << "ids\n";
+	std::tuple<uint64_t, double, double, char> item = std::make_tuple(0, 0, 0, 0);
+	for(uint64_t id : ids) {
+		if(!qt.getItem(item, id)) {
+			std::cerr << "failed " << id << "\n";
+		} else {
+			std::cerr << "found: " << id << ", " << std::get<0>(item) << ", " << std::get<1>(item) << ", " << std::get<2>(item) << ", " << std::get<3>(item) << "\n";
+			qt.updateItem(std::make_tuple(id, std::get<1>(item), std::get<2>(item), 'x'), id);
+			qt.getItem(item, id);
+			std::cerr << "updated: " << id << ", " << std::get<0>(item) << ", " << std::get<1>(item) << ", " << std::get<2>(item) << ", " << std::get<3>(item) << "\n";
+		}
+	}
 	Bounds q(4, 4, 6, 6);
-	std::list<std::tuple<double, double, char> > lst;
+	std::list<std::tuple<uint64_t, double, double, char> > lst;
 	qt.search(q, std::back_inserter(lst));
 	std::cerr << "search\n";
 	for(const auto& it : lst)
