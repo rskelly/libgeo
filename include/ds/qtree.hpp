3bdd112ba70869ca511533a9aa3d654037cd9a05
rob <rob@dijital.ca>
2017-04-19 14:23:27 -0700

    Changed qtree to simpler grid.

--- include/ds/qtree.hpp
+++ include/ds/qtree.hpp
@@ -3,30 +3,32 @@
 
 #include "util.hpp"
 #include <unordered_map>
+#include <unordered_set>
+#include <vector>
 
 using namespace geo::util;
 
-static uint64_t s_idx = 0;
-static uint64_t s_bufSize = 64;
+static size_t s_idx = 0;
+static size_t s_bufSize = 64;
 
 template <class T>
 class CatItem {
 private:
-	uint64_t m_idx;
-	uint64_t m_iidx;
-	uint64_t m_iiidx;
+	size_t m_idx;
+	size_t m_iidx;
+	size_t m_iiidx;
 
 public:
-	uint64_t count;
-	std::vector<std::pair<uint64_t, uint64_t> > items;
+	size_t count;
+	std::vector<std::pair<size_t, size_t> > items;
 
 	CatItem() : count(0) {}
 
-	uint64_t newPosition() {
+	size_t newPosition() {
 		if(items.empty() || (count % s_bufSize) == 0)
 			items.push_back(std::make_pair(0, s_idx++));
-		std::pair<uint64_t, uint64_t>& item = items[items.size() - 1];
-		uint64_t pos = item.second * s_bufSize * sizeof(T) + item.first * sizeof(T);
+		std::pair<size_t, size_t>& item = items[items.size() - 1];
+		size_t pos = item.second * s_bufSize * sizeof(T) + item.first * sizeof(T);
 		item.first++;
 		++count;
 		return pos;
@@ -38,7 +40,7 @@ public:
 		m_iiidx = 0;
 	}
 
-	bool nextPosition(uint64_t* idx) {
+	bool nextPosition(size_t* idx) {
 		while(m_idx < items.size()) {
 			if(m_iidx < items[m_idx].first) {
 				*idx = items[m_idx].second * s_bufSize * sizeof(T) + m_iidx * sizeof(T);
@@ -56,41 +58,71 @@ public:
 template <class T>
 class QTree {
 private:
+	// The geographic boundary corresponding to this tree. Will be reshaped to a square 
+	// using the longer side.
 	Bounds m_bounds;
-	int m_bins;
-	std::unordered_map<uint64_t, CatItem<T> > m_catalog;
+	// The number of bins along each side.
+	size_t m_bins;
+	// The length of one side in map units.
+	double m_size; 
+	// A catalog to keep pointers into the mapped memory.
+	std::unordered_map<size_t, CatItem<T> > m_catalog;
+	// The mapped memory.
 	MappedFile m_mapped;	
 
 public:
-	QTree(const Bounds& bounds, int bins) :
+	QTree(const Bounds& bounds, size_t bins) :
 		m_bounds(bounds), 
 		m_bins(bins) {
+
+		m_size = g_max(m_bounds.width(), m_bounds.height());
+		m_bounds.set(m_bounds.midx() - m_size / 2, m_bounds.midy() - m_size / 2, 
+			m_bounds.midx() + m_size / 2, m_bounds.midy() + m_size / 2);
 	}
 
-	uint64_t toBinX(double x) {
-		return (uint64_t) (x - m_bounds.minx()) / m_bounds.width() * m_bins;
+	const Bounds& bounds() const {
+		return m_bounds;
 	}
 
-	uint64_t toBinY(double y) {
-		return (uint64_t) (y - m_bounds.miny()) / m_bounds.height() * m_bins;
+	size_t toBinX(double x) {
+		return (size_t) (x - m_bounds.minx()) / m_size * m_bins;
 	}
 
-	uint64_t morton(double x, double y) {
-		uint64_t ix = toBinX(x);
-		uint64_t iy = toBinY(y);
-		uint64_t out = 0;
-		int shift = 1;
-		for(int i = 0; i < 31; ++i) {
-			out |= ((iy >> i) & 0xf) << shift;
-			out |= ((ix >> i) & 0xf) << (shift - 1);
-			++shift;
+	size_t toBinY(double y) {
+		return (size_t) (y - m_bounds.miny()) / m_size * m_bins;
+	}
+
+	size_t index(double x, double y) {
+		return toBinY(y) * m_bins + toBinX(x);
+	}
+
+	std::list<size_t> indices(double x, double y, double radius, bool outer = false) {
+		size_t b0 = g_max(0, toBinX(x - radius));
+		size_t b2 = g_min(m_bins, toBinX(x + radius));
+		size_t b1 = g_max(0, toBinY(y - radius));
+		size_t b3 = g_min(m_bins, toBinY(y + radius));
+		std::list<size_t> lst;
+		if(outer) {
+			for(size_t a = b0; a <= b2; ++a)
+				lst.push_back(b1 * m_bins + a);
+			for(size_t b = b1 + 1; b <= b3 - 1; ++b) {
+				lst.push_back(b * m_bins + b0);
+				lst.push_back(b * m_bins + b2);
+			}
+			for(size_t a = b0; a <= b2; ++a)
+				lst.push_back(b3 * m_bins + a);
+		} else {
+			for(size_t b = b1; b <= b3; ++b)
+				for(size_t a = b0; a <= b2; ++a) {
+					lst.push_back(b * m_bins + a);
+			}
 		}
-		return out;
+		return lst;
 	}
 
 	void addItem(double x, double y, const T& item) {
-		uint64_t bin = morton(x, y);
-		uint64_t pos = m_catalog[bin].newPosition();
+		size_t idx = index(x, y);
+		size_t pos = m_catalog[idx].newPosition();
 		if(m_mapped.size() < pos + sizeof(T))
 			m_mapped.reset(pos + sizeof(T));
 		char* data = (char*) m_mapped.data() + pos;
@@ -99,26 +131,101 @@ public:
 
 	template <class U>
 	void search(double x, double y, double radius, U output) {
-		uint64_t min = morton(x - radius, y - radius);
-		uint64_t max = morton(x + radius, y + radius);
-		radius *= radius;
-		for(uint64_t i = min; i <= max; ++i) {
+		if(!m_bounds.contains(x, y))
+			return;
+		std::list<size_t> idx = indices(x, y, radius);
+		char* data = (char*) m_mapped.data();
+		radius = g_sq(radius);
+		for(const size_t& i : idx) {
 			if(m_catalog.find(i) != m_catalog.end()) {
 				CatItem<T>& ci = m_catalog[i];
 				ci.reset();
-				uint64_t pos = 0;
+				size_t pos = 0;
 				T item;
 				while(ci.nextPosition(&pos)) {
 					if(pos + sizeof(T) > m_mapped.size())
 						g_runerr("Requested position is out of bounds: " << pos);
-					char* data = (char*) m_mapped.data() + pos;
-					std::memcpy(&item, data, sizeof(T));
-					if(std::pow(item.x - x, 2) + std::pow(item.y - y, 2) <= radius) {
+					std::memcpy(&item, data + pos, sizeof(T));
+					if(g_sq(item.x - x) + g_sq(item.y - y) <= radius) {
 						*output = item;
 						++output;
 					}
 				}
 			}
+		}
+	}
+
+	template <class U>
+	void search(const Bounds& bounds, U output) {
+		if(!m_bounds.intersects(bounds))
+			return;
+		std::list<size_t> idx = indices(bounds.midx(), bounds.midy(), g_max(bounds.width(), bounds.height()));
+		char* data = (char*) m_mapped.data();
+		for(const size_t& i : idx) {
+			if(m_catalog.find(i) != m_catalog.end()) {
+				CatItem<T>& ci = m_catalog[i];
+				ci.reset();
+				size_t pos = 0;
+				T item;
+				while(ci.nextPosition(&pos)) {
+					if(pos + sizeof(T) > m_mapped.size())
+						g_runerr("Requested position is out of bounds: " << pos);
+					std::memcpy(&item, data + pos, sizeof(T));
+					if(bounds.contains(item.x, item.y)) {
+						*output = item;
+						++output;
+					}
+				}
+			}
+		}
+	}
+
+	struct _sorter {
+		double x, y;
+		_sorter(double x, double y) : x(x), y(y) {}
+		bool operator()(const T& a, const T& b) {
+			return  (g_sq(x - a.x) + g_sq(y - a.y)) < (g_sq(x - b.x) + g_sq(y - b.y));
+		}
+	};
+
+	template <class U>
+	void nearest(double x, double y, size_t n, U output) {
+		char* data = (char*) m_mapped.data();
+		std::vector<T> tmp;
+		size_t radius = 1;
+		while(radius <= m_bins) {
+			
+			std::list<size_t> idx = indices(x, y, radius, true); // Only the outer ring; inner ring already searched.
+
+			for(const size_t& i : idx) {
+				
+				if(m_catalog.find(i) != m_catalog.end()) {
+					CatItem<T>& ci = m_catalog[i];
+					ci.reset();
+					size_t pos = 0;
+					T item;
+					while(ci.nextPosition(&pos)) {
+						if(pos + sizeof(T) > m_mapped.size())
+							g_runerr("Requested position is out of bounds: " << pos);
+						std::memcpy(&item, data + pos, sizeof(T));
+						tmp.push_back(std::move(item));
+					}
+				}
+			}
+
+			if(tmp.size() >= n) {
+				if(tmp.size() > n) {
+					struct _sorter sorter(x, y);
+					std::sort(tmp.begin(), tmp.end(), sorter);
+				}
+				for(size_t i = 0; i < n; ++i) {
+					*output = std::move(tmp[0]);
+					++output;
+				}
+				break;
+			} 
+
+			++radius;
 		}
 	}
 
--- src/tests/qtree.cpp
+++ src/tests/qtree.cpp
@@ -14,16 +14,58 @@ public:
 
 int main(int argc, char** argv) {
 	Bounds b(0, 0, 100, 100);
-	QTree<PPoint> t(b, 10);
+
+	double sx = Util::random(0, 100);
+	double sy = Util::random(0, 100);
+	double sr = 10.0;
+	int sc = 0;
+
+	QTree<PPoint> t(b, 1000);
+	Stopwatch sw;
+	sw.start();
 	for(int i = 0; i < 1000000; ++i) {
 		double x = Util::random(0, 100);
 		double y = Util::random(0, 100);
 		t.addItem(x, y,  PPoint(x, y));
 		//std::cerr << "added: " << x << ", " << y << "\n";
+		if(g_sq(x - sx) + g_sq(y - sy) <= g_sq(sr)) {
+			//std::cerr << "1," << x << ", " << y << "\n";
+			++sc;
+		}
 	}
 
+	sw.stop();
+	std::cerr << "added 1000000 in " << sw.millis() << "\n";
+
 	std::list<PPoint> res;
-	t.search(50, 50, 50, std::back_inserter(res));
-	for(const PPoint& tt : res)
-		std::cerr << "found: " << tt.x << ", " << tt.y << "\n";
+	t.search(sx, sy, sr, std::back_inserter(res));
+	std::cerr << "found " << res.size() << " items out of " << sc << "\n";
+	//for(const PPoint& p : res)
+		//std::cerr << "2," << p.x << ", " << p.y << "\n";
+
+	for(int i = 0; i < 10; ++i) {
+		res.clear();
+		sw.reset();
+		sw.start();
+		t.search(Util::random(0, 100), Util::random(0, 100), 5, std::back_inserter(res));
+		sw.stop();
+		std::cerr << "found by search " << res.size() << " in " << sw.millis() << "\n";	
+		//for(const PPoint& tt : res)
+		//	std::cerr << "found: " << tt.x << ", " << tt.y << "\n";
+	}
+
+	for(int i = 0; i < 10; ++i) {
+		double x = Util::random(0, 100);
+		double y = Util::random(0, 100);
+		res.clear();
+		sw.reset();
+		sw.start();
+		t.nearest(x, y, 1, std::back_inserter(res));
+		sw.stop();
+		PPoint& d = res.front();
+		double dist = std::sqrt(g_sq(x - d.x) + g_sq(y - d.y));
+		std::cerr << "found nearest " << res.size() << " in " << sw.millis() << " at " << dist << "\n";	
+		//for(const PPoint& tt : res)
+		//	std::cerr << "found: " << tt.x << ", " << tt.y << "\n";
+	}
 }
