d8c6b20666dac514c17dbcaff651ec8cba53e726
rob <rob@dijital.ca>
2018-01-27 17:19:21 -0800

    Changed to Boost UUID (old one didn't do anything.)

--- include/crypto/uuid.hpp
+++ include/crypto/uuid.hpp
@@ -10,13 +10,14 @@
 #ifndef LIBGEO_INCLUDE_CRYPTO_UUID_HPP_
 #define LIBGEO_INCLUDE_CRYPTO_UUID_HPP_
 
+#include <boost/uuid/uuid.hpp>            // uuid class
+#include <boost/uuid/uuid_generators.hpp> // generators
+#include <boost/uuid/uuid_io.hpp>         // streaming operators etc.
+
 #include <string>
-#include <cstdlib>
 
 namespace geo {
 namespace crypto {
-
-const std::string CHARS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
 /**
  * Provides UUID strings.
@@ -28,27 +29,8 @@ public:
 	 * Return a unique identifier string.
 	 */
 	static std::string uuid(){
-	  std::string uuid = std::string(36,' ');
-	  int rnd = 0;
-	  int r = 0;
-
-	  uuid[8] = '-';
-	  uuid[13] = '-';
-	  uuid[18] = '-';
-	  uuid[23] = '-';
-
-	  uuid[14] = '4';
-
-	  for(int i=0;i<36;i++){
-		if (i != 8 && i != 13 && i != 18 && i != 14 && i != 23) {
-		  if (rnd <= 0x02) {
-			  rnd = 0x2000000 + (std::rand() * 0x1000000) | 0;
-		  }
-		  rnd >>= 4;
-		  uuid[i] = CHARS[(i == 19) ? ((rnd & 0xf) & 0x3) | 0x8 : rnd & 0xf];
-		}
-	  }
-	  return uuid;
+		boost::uuids::uuid uuid = boost::uuids::random_generator()();
+		return boost::uuids::to_string(uuid);
 	}
 };
 
