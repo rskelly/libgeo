5a37a2d102406df6cb02d676a2c95a09596e3b9c
Rob <rob@dijital.ca>
2018-09-30 19:23:36 -0700

    Removed status messages from header.

--- ann/include/ANN/ANN.h
+++ ann/include/ANN/ANN.h
@@ -59,9 +59,6 @@
 #ifndef ANN_H
 #define ANN_H
 
-#pragma message("win32?")
-#pragma message(WIN32)
-
 #ifdef WIN32
   //----------------------------------------------------------------------
   // For Microsoft Visual C++, externally accessible symbols must be
