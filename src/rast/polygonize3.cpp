0316ad642300a3af3700b9354a1224c15dfb0aa4
rob <rob@dijital.ca>
2018-11-26 10:17:42 -0800

    Polygonizing works great.

--- src/rast/polygonize3.cpp
+++ src/rast/polygonize3.cpp
@@ -32,25 +32,19 @@ using namespace geo::raster;
 using namespace geos::geom;
 using namespace geos::operation::geounion;
 
-std::atomic<long> __fid(0);
-std::mutex __gmtx;  // For the geoms map.
-std::mutex __idmtx; // For the final list.
-std::mutex __omtx;  // For OGR writes.
-std::condition_variable __cv;
+std::atomic<long> __fid(0);		// A feature ID for geometries.
+std::mutex __gmtx;  			// For the geoms map.
+std::mutex __fmtx; 			// For the final list.
+std::mutex __omtx;  			// For OGR writes.
+std::condition_variable __cv;	// For waiting on the queue.
 
-std::string _finishStatus(int c, int final) {
-	std::stringstream ss;
-	ss << "Finishing " << c << " of " << final << " polygons";
-	return ss.str();
-}
-
-void _writeToFile(std::unordered_map<int, std::vector<Polygon*> >* geoms, std::set<int>* finalIds, OGRLayer* layer,
-		GeometryFactory::unique_ptr* fact, GEOSContextHandle_t* gctx, bool* running, Status* status, int* finalCount) {
+// Write to the file from the map of geometry lists.
+// On each loop, extracts a single finalized poly ID and loads those polys for unioning.
+void _writeToFile(std::unordered_map<int, std::vector<Polygon*> >* geoms, std::set<int>* finalIds,
+		OGRLayer* layer, GeometryFactory::unique_ptr* fact, GEOSContextHandle_t* gctx,
+		bool* running) {
 
 	while(*running || !finalIds->empty()) {
-
-		if(!*running)
-			status->update(0.5 + (float) finalIds->size() / *finalCount, _finishStatus(finalIds->size(), *finalCount));
 
 		std::vector<Polygon*> polys;
 		Geometry* geom;
@@ -58,16 +52,18 @@ void _writeToFile(std::unordered_map<int, std::vector<Polygon*> >* geoms, std::s
 
 		// Get an ID and the list of polys from the queue.
 		{
-			std::unique_lock<std::mutex> lk(__idmtx);
+			std::unique_lock<std::mutex> lk(__fmtx);
 			// Wait for a notification if the queue is empty.
 			while(*running && finalIds->empty())
 				__cv.wait(lk);
+			// If the wakeup is spurious, skip.
 			if(finalIds->empty())
 				continue;
 			// Get the ID.
 			id = *(finalIds->begin());
 			finalIds->erase(id);
 		}
+		// Get the list of polys and remove the list from the map.
 		{
 			std::lock_guard<std::mutex> lk(__gmtx);
 			// If the geoms list is still here, grab it.
@@ -79,6 +75,7 @@ void _writeToFile(std::unordered_map<int, std::vector<Polygon*> >* geoms, std::s
 
 		if(polys.empty())
 			continue;
+
 		// Union the polys. If the result is not a multi, make it one.
 		geom = geos::operation::geounion::CascadedPolygonUnion::CascadedPolygonUnion::Union(&polys);
 		polys.clear();
@@ -97,11 +94,11 @@ void _writeToFile(std::unordered_map<int, std::vector<Polygon*> >* geoms, std::s
 		feat.SetField("id", (GIntBig) id);
 		feat.SetFID(++__fid);
 
+		// Write to the output file.
 		int err;
 		{
 			std::lock_guard<std::mutex> lk(__omtx);
 			err = layer->CreateFeature(&feat);
-			//std::cout << "poly " << std::this_thread::get_id() << "\n";
 		}
 
 		// Delete the polys.
@@ -112,10 +109,9 @@ void _writeToFile(std::unordered_map<int, std::vector<Polygon*> >* geoms, std::s
 		if(OGRERR_NONE != err)
 			g_runerr("Failed to add geometry.");
 	}
-
-	std::cout << "Finished " << std::this_thread::get_id() << "\n";
 }
 
+// Produce a rectangular polygon from the four corners.
 Polygon* _makeGeom(double x0, double y0, double x1, double y1, GeometryFactory::unique_ptr* fact) {
 	// Build the geometry.
 	CoordinateSequence* seq = (*fact)->getCoordinateSequenceFactory()->create(5, 2);
@@ -128,6 +124,7 @@ Polygon* _makeGeom(double x0, double y0, double x1, double y1, GeometryFactory::
 	return (*fact)->createPolygon(ring, NULL);
 }
 
+// Make an OGR database to write the polygons to.
 std::tuple<GDALDataset*, OGRLayer*> _makeDataset(const std::string& filename, const std::string& driver, const std::string& layerName, int srid) {
 	// Get the vector driver.
 	GDALDriver *drv = GetGDALDriverManager()->GetDriverByName(driver.c_str());
@@ -163,6 +160,7 @@ std::tuple<GDALDataset*, OGRLayer*> _makeDataset(const std::string& filename, co
 	return std::make_tuple(ds, layer);
 }
 
+// Produce a status message from the current and total row counds.
 std::string _rowStatus(int r, int rows) {
 	std::stringstream ss;
 	ss << "Row " << (r + 1) << " of " << rows;
@@ -177,9 +175,13 @@ void Grid::polygonize(const std::string& filename, const std::string& layerName,
 	if(!props().isInt())
 		g_runerr("Only int rasters can be polygonized.");
 
+	if(threads < 1)
+		threads = 1;
+
 	std::unique_ptr<MemRaster> src;
 	std::unique_ptr<MemRaster> msk;
 
+	// Create a memory-mapped version of the input raster.
 	{
 		GridProps mprops(props());
 		mprops.setBands(1);
@@ -187,100 +189,126 @@ void Grid::polygonize(const std::string& filename, const std::string& layerName,
 		writeTo(*src, mprops.cols(), mprops.rows(), 0, 0, 0, 0, band, 1);
 	}
 
+	// Extract some grid properties.
 	const GridProps& props = src->props();
 	int cols = props.cols();
 	int rows = props.rows();
 	double resX = props.resolutionX();
 	double resY = props.resolutionY();
 	const Bounds& bounds = props.bounds();
+
+	// Create a buffer for the row.
 	GridProps rowProps(props);
 	rowProps.setSize(cols, 1);
 	MemRaster buf(rowProps, false);
 
+	// The starting corner coordinates.
 	double startX = resX > 0 ? bounds.minx() : bounds.maxx();
 	double startY = resY > 0 ? bounds.miny() : bounds.maxy();
+
+	// Perturbation values for unioning geometries.
 	double epsX = resX > 0 ? std::numeric_limits<double>::min() : -std::numeric_limits<double>::min();
 	double epsY = resY > 0 ? std::numeric_limits<double>::min() : -std::numeric_limits<double>::min();
 
+	// Create the output dataset
 	GEOSContextHandle_t gctx = OGRGeometry::createGEOSContext();
 	GeometryFactory::unique_ptr fact = GeometryFactory::create(new PrecisionModel());
-
 	std::unique_ptr<GDALDataset> ds;
 	std::unique_ptr<OGRLayer> layer;
-
 	{
 		std::tuple<GDALDataset*, OGRLayer*> t = _makeDataset(filename, driver, layerName, srid);
 		ds.reset(std::get<0>(t));
 		layer.reset(std::get<1>(t));
 	}
 
+	// Data containers.
 	std::unordered_map<int, std::vector<Polygon*> > geoms;
 	std::set<int> activeIds;
 	std::set<int> finalIds;
+
+	// Thread control features.
 	bool running = true;
-	int finalCount = 0;
 	std::list<std::thread> ths;
 
-	for(int i = 0; i < 4; ++i)
-		ths.emplace_back(&_writeToFile, &geoms, &finalIds, layer.get(), &fact, &gctx, &running, &status, &finalCount);
+	// Start output threads.
+	for(int i = 0; i < threads; ++i)
+		ths.emplace_back(&_writeToFile, &geoms, &finalIds, layer.get(), &fact, &gctx, &running);
 
+	// Process raster.
 	for(int r = 0; r < rows; ++r) {
 
-		status.update((float) r / rows / 2, _rowStatus(r, rows));
+		status.update((float) r / rows, _rowStatus(r, rows));
 
+		// Load the row buffer.
 		src->writeTo(buf, cols, 1, 0, r, 0, 0, 1, 1);
 
+		// Initialize the corner coordinates.
 		double x0 = startX;
 		double y0 = startY + r * resY;
 		double x1 = x0;
 		double y1 = y0 + resY + epsY; // Perturbation for intersection to work.
 
+		// For tracking cell values.
 		int v0 = buf.getInt(0, 0, 1);
 		int v1 = -1;
 
+		// Reset the list of IDs extant in the current row.
 		activeIds.clear();
 
 		for(int c = 1; c < cols; ++c) {
+
+			// If the current cell value differs from the previous one...
 			if((v1 = buf.getInt(c, 0, 1)) != v0) {
-
+				// Update the right x coordinate.
 				x1 = startX + c * resX + epsX;
-
+				// If the value is a valid ID, create and the geometry and save it for writing.
 				if(v0 > 0) {
 					geoms[v0].push_back(_makeGeom(x0, y0, x1, y1, &fact));
 					activeIds.insert(v0);
 				}
-
+				// Update values for next loop.
 				v0 = v1;
 				x0 = x1;
 			}
 		}
 
+		// IDs that are in the geoms array and not in the current row are ready to be finalized.
 		{
-			std::lock_guard<std::mutex> lk(__gmtx);
-			for(auto& it : geoms) {
+			std::lock_guard<std::mutex> lk0(__gmtx);
+			std::lock_guard<std::mutex> lk1(__fmtx);
+			for(const auto& it : geoms) {
 				if(activeIds.find(it.first) == activeIds.end())
 					finalIds.insert(it.first);
 			}
 		}
 		__cv.notify_all();
 
+		while(!finalIds.empty()) {
+			//std::this_thread::sleep_for(std::chrono::duration<double, std::milli>(1));
+			std::this_thread::yield();
+		}
 	}
 
+	// Finalize all remaining geometries.
 	{
-		std::lock_guard<std::mutex> lk(__gmtx);
+		std::lock_guard<std::mutex> lk0(__gmtx);
+		std::lock_guard<std::mutex> lk1(__fmtx);
 		for(auto& it : geoms)
 			finalIds.insert(it.first);
 	}
 
+	// Let the threads shut down when they run out of geometries.
 	running = false;
-	finalCount = finalIds.size();
-
 	__cv.notify_all();
 
 	for(std::thread& th : ths) {
 		if(th.joinable())
 			th.join();
 	}
+
+	// Release the layer -- GDAL will take care of it. But close the dataset so that can happen.
+	layer.release();
+	GDALClose(ds.release());
 
 	status.update(1.0f, "Finished");
 
