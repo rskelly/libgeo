5fceeb8ca69c92578956661474e5fe07de414b8b
rob <rob@dijital.ca>
2019-06-05 11:23:59 -0700

    pcnorm with barycentric interp. Works good.

--- src/apps/pcnorm.cpp
+++ src/apps/pcnorm.cpp
@@ -1,77 +1,235 @@
 /*
- * polygonize.cpp
+ * pcnorm2.cpp
  *
- *  Created on: Apr 13, 2017
+ *  Created on: May 5, 2019
  *      Author: rob
  */
 
-#include <pointcloud.hpp>
+#include <limits>
 #include <fstream>
-#include <vector>
-#include <unordered_set>
-#include <iostream>
 
-#include "pointcloud.hpp"
+#include <liblas/liblas.hpp>
 
-void usage() {
-	std::cerr << "Usage: pcnorm [options] <output dir> <terrain model> <input las [*]>\n"
-			<< " Point filtering parameters:\n"
-			<< " -c <class(es)>  Comma-delimited list of classes to keep.\n"
-			<< " -t <threshold>  The minimum height threshold.\n"
-			<< " -b <band>       The band in the terrain model.\n"
-			<< " -f              Force overwrite of existing files.\n";
+void getBounds(liblas::Reader& rdr, double* bounds) {
+
+	bounds[0] = bounds[2] = bounds[4] = std::numeric_limits<double>::max();
+	bounds[1] = bounds[3] = bounds[5] = std::numeric_limits<double>::lowest();
+
+	double x, y, z;
+
+	rdr.Reset();
+	while(rdr.ReadNextPoint()) {
+		const liblas::Point& pt = rdr.GetPoint();
+		x = pt.GetX();
+		y = pt.GetY();
+		z = pt.GetZ();
+		if(x < bounds[0]) bounds[0] = x;
+		if(x > bounds[1]) bounds[1] = x;
+		if(y < bounds[2]) bounds[2] = y;
+		if(y > bounds[3]) bounds[3] = y;
+		if(z < bounds[4]) bounds[4] = z;
+		if(z > bounds[5]) bounds[5] = z;
+	}
+}
+
+void buildGrid(liblas::Reader& rdr, double* bounds, double res, int& cols, int& rows, std::vector<float>& grid) {
+
+	// Increase bounds enough to add 2 cells all around.
+	bounds[0] -= res;
+	bounds[1] += res;
+	bounds[2] -= res;
+	bounds[3] += res;
+
+	// Get grid size.
+	cols = (int) std::ceil((bounds[1] - bounds[0]) / res);
+	rows = (int) std::ceil((bounds[3] - bounds[2]) / res);
+
+	std::vector<float> weights(cols * rows);
+
+	{
+		// To compute weighted mean, need weights and values arrays.
+		grid.resize(cols * rows);
+		std::fill(grid.begin(), grid.end(), 0);
+		std::fill(weights.begin(), weights.end(), 0);
+
+		double px, py, pz;
+		double x, y, d, w;
+		int col, row;
+		double rad = std::pow(res, 2);
+
+		rdr.Reset();
+		while(rdr.ReadNextPoint()) {
+
+			const liblas::Point& pt = rdr.GetPoint();
+			if(pt.GetClassification().GetClass() != 2)
+				continue;
+
+			px = pt.GetX();
+			py = pt.GetY();
+			pz = pt.GetZ();
+			col = (int) (px - bounds[0]) / res;
+			row = (int) (py - bounds[2]) / res;
+
+			for(int r = row - 1; r < row + 2; ++r) {
+				for(int c = col - 1; c < col + 2; ++c) {
+					if(c >= 0 && r >= 0 && c < cols && r < rows) {
+
+						x = bounds[0] + (c * res) + res * 0.5;
+						y = bounds[2] + (r * res) + res * 0.5;
+						d = std::pow(x - px, 2.0) + std::pow(y - py, 2.0);
+
+						if(d > rad)
+							continue;
+
+						w = 1.0 - d / rad;
+
+						// Accumulate the weighted heights and weights.
+						grid[r * cols + c] += pz * w;
+						weights[r * cols + c] += w;
+					}
+				}
+			}
+		}
+	}
+
+	// Normalize by weights.
+	for(size_t i = 0; i < grid.size(); ++i) {
+		if(weights[i] > 0)
+			grid[i] /= weights[i];
+	}
+
+	// Fill in zeroes.
+	for(size_t i = 0; i < grid.size(); ++i) {
+		if(weights[i] == 0) {
+			double x, y, d, w0, rad, w = 0, s = 0;
+			int o = 2;
+			int col = i % cols;
+			int row = i / cols;
+			double cx = bounds[0] + col * res + res * 0.5;
+			double cy = bounds[2] + row * res + res * 0.5;
+			do {
+				rad = std::pow(o * res, 2);
+				for(int r = row - o; r < row + o + 1; ++r) {
+					for(int c = col - o; c < col + o + 1; ++c) {
+						if(c >= 0 && r >= 0 && c < cols && r < rows) {
+
+							if(weights[r * cols + c] == 0)
+								continue;
+
+							x = bounds[0] + (c * res) + res * 0.5;
+							y = bounds[2] + (r * res) + res * 0.5;
+							d = std::pow(x - cx, 2.0) + std::pow(y - cy, 2.0);
+
+							if(d > rad)
+								continue;
+
+							w0 = 1.0 - d / rad;
+
+							// Accumulate the weighted heights and weights.
+							s += grid[r * cols + c] * w0;
+							w += w0;
+						}
+					}
+				}
+				++o;
+			} while(w == 0);
+
+			grid[i] = s / w;
+		}
+	}
+}
+
+double bary(double x, double y,
+		double x0, double y0, double z0,
+		double x1, double y1, double z1,
+		double x2, double y2, double z2) {
+	double w0 = ((y1 - y2) * (x - x2) + (x2 - x1) * (y - y2)) / ((y1 - y2) * (x0 - x2) + (x2 - x1) * (y0 - y2));
+	double w1 = ((y2 - y0) * (x - x2) + (x0 - x2) * (y - y2)) / ((y1 - y2) * (x0 - x2) + (x2 - x1) * (y0 - y2));
+	double w2 = 1 - w0 - w1;
+	return (w0 * z0) + (w1 * z1) + (w2 * z2);
+}
+
+void normalize(liblas::Reader& rdr, liblas::Writer& wtr, double* bounds, double res, int cols, int rows, std::vector<float>& grid) {
+
+	bounds[4] = std::numeric_limits<double>::max();
+	bounds[5] = std::numeric_limits<double>::lowest();
+
+	double z, px, py, pz, cx0, cy0, cz0, cx1, cy1, cz1, cx2, cy2, cz2, nz;
+	int col, row, col0, row0;
+
+	rdr.Reset();
+	while(rdr.ReadNextPoint()) {
+		const liblas::Point& pt = rdr.GetPoint();
+		px = pt.GetX();
+		py = pt.GetY();
+		pz = pt.GetZ();
+		// Point's home cell.
+		col = (int) (px - bounds[0]) / res;
+		row = (int) (py - bounds[2]) / res;
+		// Center of home cell.
+		cx0 = bounds[0] + (col * res) + res * 0.5;
+		cy0 = bounds[2] + (row * res) + res * 0.5;
+		cz0 = grid[row * cols + col];
+		// Cell offsets.
+		col0 = px < cx0 ? col - 1 : col + 1;
+		row0 = py < cy0 ? row - 1 : row + 1;
+		// Centers of offset cells.
+		cx1 = bounds[0] + (col0 * res) + res * 0.5;
+		cy1 = bounds[2] + (row * res) + res * 0.5;
+		cz1 = grid[row * cols + col0];
+		cx2 = bounds[0] + (col * res) + res * 0.5;
+		cy2 = bounds[2] + (row0 * res) + res * 0.5;
+		cz2 = grid[row0 * cols + col];
+
+		if(cz0 == -9999.0 || cz1 == -9999.0 || cz2 == -9999.0)
+			continue;
+
+		// Get the barycentric z
+		nz = bary(px, py, cx0, cy0, cz0, cx1, cy1, cz1, cx2, cy2, cz2);
+		// Make point and write it.
+		liblas::Point pt0(pt);
+		pt0.SetZ((z = pz - nz));
+		wtr.WritePoint(pt0);
+		// Adjust bounds.
+		if(z < bounds[4]) bounds[4] = z;
+		if(z > bounds[5]) bounds[5] = z;
+	}
 }
 
 int main(int argc, char** argv) {
 
-	if(argc < 3) {
-		usage();
-		return 1;
-	}
+	std::string infile = argv[1];
+	std::string outfile = argv[2];
+	double resolution = atof(argv[3]);
 
-	std::vector<std::string> args;
-	std::vector<int> classes;
-	geo::pc::PCPointFilter filter;
-	int band = 1;
-	bool force = false;
+	std::ifstream input(infile);
+	liblas::ReaderFactory rf;
+	liblas::Reader rdr = rf.CreateWithStream(input);
+	const liblas::Header& rhdr = rdr.GetHeader();
 
-	for(int i = 1; i < argc; ++i) {
-		std::string v = argv[i];
-		if(v == "-c") {
-			std::vector<std::string> tmp;
-			std::string cls = argv[++i];
-			Util::splitString(std::back_inserter(tmp), cls);
-			for(const std::string& t : tmp)
-				classes.push_back(atoi(t.c_str()));
-			filter.addClassFilter(classes);
-		} else if(v == "-t") {
-			filter.addZRangeFilter(atof(argv[++i]), DBL_MAX);
-		} else if(v == "-f") {
-			force = true;
-		} else if(v == "-b") {
-			band = atoi(argv[++i]);
-		} else {
-			args.push_back(argv[i]);
-		}
-	}
+	liblas::Header whdr(rhdr);
 
-	if(args.size() < 3) {
-		std::cerr << "Output dir, terrain model and input files are required.\n";
-		usage();
-		return 1;
-	}
+	double bounds[6];
+	getBounds(rdr, bounds);
 
-	std::vector<std::string> infiles(args.begin() + 2, args.end());
+	double gbounds[6];
+	for(int i = 0; i < 6; ++i)
+		gbounds[i] = bounds[i];
 
-	try {
-		geo::pc::Normalizer n(infiles);
-		n.setFilter(filter);
-		n.normalize(args[1], args[0], band, force);
-	} catch(const std::exception& ex) {
-		std::cerr << ex.what() << "\n";
-		usage();
-	}
-	return 0;
+	int cols, rows;
+	std::vector<float> grid;
+	buildGrid(rdr, gbounds, resolution, cols, rows, grid);
+
+	std::ofstream output;
+	liblas::WriterFactory wf;
+	liblas::Create(output, outfile);
+	liblas::Writer wtr(output, whdr);
+
+	normalize(rdr, wtr, gbounds, resolution, cols, rows, grid);
+
+	whdr.SetMin(bounds[0], bounds[2], gbounds[4]);
+	whdr.SetMax(bounds[1], bounds[3], gbounds[5]);
+
+	wtr.SetHeader(whdr);
+
 }
-
-
